87850dc964fad567bf38c84d8a468e5ab31543a1
Mark Ma <goldffd700@gmail.com>
2025-12-15 18:18:09 +0800

    refactor: 移除未用到的檔案

--- src/content/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-const App = () => (
-    <div>
-        Hello Gemini Folder Extension Content
-    </div>
-);
-
-export default App;
--- src/popup/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-const App = () => (
-    <div>
-        Hello Gemini Folder Extension Popup
-    </div>
-);
-
-export default App;
